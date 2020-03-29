1f0f8423b0fe0bf19bd23d527aa668d7657200f0
Sean <seanchang@lafresh.com.tw>
2020-03-29 17:41:38 +0800

    save open times data and make Toast

--- app/src/main/java/tw/sean/codedemo/MainActivity.java
+++ app/src/main/java/tw/sean/codedemo/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +17,7 @@ import tw.sean.codedemo.model.WeatherRes;
 import tw.sean.codedemo.utils.Json;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String SP_KEY = "preferences";
     private RecyclerView rvWeather;
     private WeatherAdapter weatherAdapter;
 
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         List<WeatherRes.RecordsBean.LocationBean.WeatherElementBean.TimeBean> minTs = getWeatherData();
         weatherAdapter = new WeatherAdapter(minTs);
         initViews();
+        checkAndSaveOpenTimes();
     }
 
     private void initViews() {
@@ -41,10 +45,21 @@ public class MainActivity extends AppCompatActivity {
         List<WeatherRes.RecordsBean.LocationBean.WeatherElementBean> weatherElement =
                 res.getRecords().getLocation().get(0).getWeatherElement();
         for (WeatherRes.RecordsBean.LocationBean.WeatherElementBean bean : weatherElement) {
-            if(bean.getElementName().equals("MinT")){
+            if (bean.getElementName().equals("MinT")) {
                 return bean.getTime();
             }
         }
         return new ArrayList<>();
+    }
+
+    private void checkAndSaveOpenTimes() {
+        String openKey = "open_times";
+        SharedPreferences sp = getSharedPreferences(SP_KEY, MODE_PRIVATE);
+        int openTimes = sp.getInt(openKey, 0);
+        if (openTimes > 0) {
+            Toast.makeText(this, "歡迎回來", Toast.LENGTH_SHORT).show();
+        } else {
+            sp.edit().putInt(openKey, openTimes + 1).apply();
+        }
     }
 }
